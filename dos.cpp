9cf0acdb91dd231632478d332c37554d1d963453
Emisproo <proomes23@gmail.com>
2021-06-09 15:59:00 -0300

    commit desde branch

--- dos.cpp
+++ dos.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main ()
 { 
-    cout << "Hola soy dos.cpp, antes era uno.cpp y era feliz";
+    cout << "Hola soy dos.cpp, antes era uno.cpp y era feliz...";
     return 0;
 }
